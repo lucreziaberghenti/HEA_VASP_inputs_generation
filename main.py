3f0274e8b7308d717dad8e6c16d782dd3b814935
lucreziaberghenti <lucreziaberghenti@gmail.com>
2024-08-12 17:12:21 +0200

    creation of saved.npy where I save the variable saved

--- main.py
+++ main.py
@@ -22,6 +22,35 @@ positions=coord.tolist()
 #saved will be an array of (3,4,5)-dim arrays representing a configuration each
 saved=np.array([],dtype=int)
 
+#check if 'saved.npy' exists
+#define the path where saved should be searched
+path = './saved.npy'
+
+if os.path.exists(path):
+    #saved is an np array of dimensions: s, nslice, nrow, ncol
+    saved=np.load('saved.npy')
+    #read the number of saved configurations
+    s=saved.shape[0]
+    
+else:
+    #number of saved configurations is zero
+    s=0
+    
+print("number of saved configurations: ",s)   
+#print("list of saved configurations:\n",saved,"\n")
+
+
+#generate inequivalent random configuration
+#generate random matrix (nslice x nrow x ncol)=(3, 4, 5) with elements 1,2,3,4,5 repeated n1,n2,n3,n4,n5 times
+#the function newConf also check that in "saved" there is not an equivalent conf to the one generated
+conf=functions.newConf(saved)
+
+#increment the counter
+s+=1
+
+print("conf n. ", s, " generated:\n",conf,"\n")
+saved=np.append(saved, conf)
+
 #generate inequivalent random configuration
 #generate random matrix (nslice x nrow x ncol)=(3, 4, 5) with elements 1,2,3,4,5 repeated n1,n2,n3,n4,n5 times
 #the function newConf also check that in "saved" there is not an equivalent conf to the one generated
@@ -32,3 +61,8 @@ species=functions.Generate_species(conf)
 
 #function that generates VASP input files
 functions.VASP_input(species,positions,s)
+
+#reshape using the update number of saved configurations s, also because when I add the new conf "saved" becomes linear
+saved=np.reshape(saved, (s,nslice,nrow,ncol))
+np.save('saved.npy',saved)
+print("updated list of saved configurations:\n", saved)
