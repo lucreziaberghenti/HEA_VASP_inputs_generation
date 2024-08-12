27a09f5111ea76b70b5edc80f3849f07f904b3e1
lucreziaberghenti <lucreziaberghenti@gmail.com>
2024-08-12 22:32:17 +0200

    translation of comments from italian to english

--- test_Generate_species.py
+++ test_Generate_species.py
@@ -4,7 +4,7 @@ from functions import Generate_species
 class TestGenerateSpeciesFunction(unittest.TestCase):
 
     def test_generate_species(self):
-        # Test con una matrice 2x2x2 con valori da 1 a 5
+        #Test with matrix 2x2x2 having values from 1 to 5
         conf = [
             [[1, 2], [3, 4]],
             [[5, 1], [2, 3]]
@@ -12,26 +12,27 @@ class TestGenerateSpeciesFunction(unittest.TestCase):
         expected_output = ['Co', 'Cr', 'Fe', 'Mn', 'Ni', 'Co', 'Cr', 'Fe']
         self.assertEqual(Generate_species(conf), expected_output)
 
-        # Test con una matrice 1x2x2 con valori da 1 a 3
+        #Test with matrix 1x2x2 having values from 1 to 3
         conf = [
             [[1, 2], [3, 1]]
         ]
         expected_output = ['Co', 'Cr', 'Fe', 'Co']
         self.assertEqual(Generate_species(conf), expected_output)
         
-        # Test con una matrice 1x1x1 con un solo valore
+        # Test with a matrix 1x1x1 having only one value
         conf = [
             [[5]]
         ]
         expected_output = ['Ni']
         self.assertEqual(Generate_species(conf), expected_output)
 
-        # Test con valori che non sono mappati
+        #Test with non mapped values
         conf = [
             [[0]]
         ]
         expected_output = ['error']
         self.assertEqual(Generate_species(conf), expected_output)
 
+#if the file is correctly executed then tests are executed
 if __name__ == '__main__':
     unittest.main()
