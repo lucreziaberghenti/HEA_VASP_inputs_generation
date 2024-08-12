a7770c7a9b45e6730585d9a3d9b6340ae1d780a6
lucreziaberghenti <lucreziaberghenti@gmail.com>
2024-08-12 22:16:16 +0200

    created and executed test on function element, test succesfully passed

---
+++ test_element_function.py
@@ -0,0 +1,30 @@
+import unittest
+from functions import element
+
+class Test_Element_Function(unittest.TestCase):
+    def test_element(self):
+
+        print("running test...")
+
+        #test of correct cases
+        self.assertEqual(element(1),'Co')
+
+        self.assertEqual(element(2),'Cr')
+
+        self.assertEqual(element(3),'Fe')
+
+        self.assertEqual(element(4),'Mn')
+
+        self.assertEqual(element(5),'Ni')
+
+        #test of input of element function outside of the range 1:5
+        self.assertEqual(element(0),'error')
+
+        self.assertEqual(element(6),'error')
+
+        self.assertEqual(element(-10),'error')
+
+
+        #if the file is correctly executed then tests are executed
+        if __name__ == '__main__':
+            unittest.main()
