2492e237bb0c4ebb13b9f2cdf7612c82f241f66a
lucreziaberghenti <lucreziaberghenti@gmail.com>
2024-08-13 19:19:41 +0200

    execute test on newConf, everything ok

--- test_newConf.py
+++ test_newConf.py
@@ -1,28 +1,32 @@
 import unittest
 import numpy as np
 from functions import newConf
+from functions import Equivalent
 
 class Test_newConf(unittest.TestCase):
 
+    # Test check on dimensions of generated conf
     def test_generate_new_conf_dimensions(self):
-        # Test per controllare la dimensione della configurazione generata
+        
         saved = []
         conf = newConf(saved)
-        self.assertEqual(conf.shape, (3, 4, 5), "La dimensione della configurazione generata è errata")
+        self.assertEqual(conf.shape, (3, 4, 5), "Wrong dimensions")
     
+    # Test on generated number of ripetions (distribution) for each element 1,2,3,4,5
     def test_generate_new_conf_distribution(self):
-        # Test per controllare la distribuzione degli elementi nella configurazione
+        
         saved = []
         conf = newConf(saved)
         
         n_expected = np.array([12, 12, 12, 12, 12])
         n_generated = np.array([np.sum(conf == i) for i in range(1, 6)])
         
-        self.assertTrue(np.array_equal(n_generated, n_expected), 
-                        "La distribuzione degli elementi nella configurazione generata è errata")
+        self.assertTrue(np.array_equal(n_generated, n_expected), "Wrong distribution of elements")
 
+
+    # Test per verificare che la nuova configurazione non sia equivalente a quelle già salvat
     def test_generate_non_equivalent_conf(self):
-        # Test per verificare che la nuova configurazione non sia equivalente a quelle già salvate
+        
         saved = [np.array([[[1, 1, 2, 2, 3],
                             [1, 2, 3, 4, 5],
                             [1, 2, 3, 4, 5],
@@ -39,8 +43,7 @@ class Test_newConf(unittest.TestCase):
         conf = newConf(saved)
         
         # Verifica che la nuova configurazione non sia equivalente a quella salvata
-        self.assertFalse(Equivalent(conf, saved), 
-                         "La nuova configurazione generata è equivalente a una già salvata")
+        self.assertFalse(Equivalent(conf, saved), "Generated configuration is equivalent to one already saved")
 
     def test_generate_unique_conf(self):
         # Test per verificare che più chiamate a newConf generino configurazioni uniche
@@ -49,8 +52,8 @@ class Test_newConf(unittest.TestCase):
         saved.append(conf1)
         conf2 = newConf(saved)
         
-        self.assertFalse(np.array_equal(conf1, conf2), 
-                         "La seconda configurazione generata è uguale alla prima, dovrebbe essere diversa")
+        self.assertFalse(np.array_equal(conf1, conf2), "The two saved configurations should be different")
 
-if _name_ == '_main_':
+#if the file is correctly executed then tests are executed
+if __name__ == '__main__':
     unittest.main()
