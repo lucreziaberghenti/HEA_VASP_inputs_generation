21ccd938ab64f25fb5c68a300c125d2574836f4e
lucreziaberghenti <lucreziaberghenti@gmail.com>
2024-08-13 15:59:06 +0200

    test on atom positions ok, solved test coordinates, test on fcc properties needs to be solved

--- test_Coordinates.py
+++ test_Coordinates.py
@@ -1,22 +1,25 @@
 import unittest
 import numpy as np
 from ase.build import fcc111
+from functions import Coordinates
 
 class TestCoordinates(unittest.TestCase):
 
+    # Test if the function returns an array of dim (60, 3)
     def test_coordinates_shape(self):
-        # Test if the function returns an array of dim (60, 3)
+        
         coords = Coordinates()
         self.assertEqual(coords.shape, (60, 3))
 
+    # Test if the first atomic positions are the expected of ones of Ni fcc(111)
     def test_atom_positions(self):
-        # Test if the first atomic positions are the expected of ones of Ni fcc(111)
+        
         expected_positions = np.array([
-            [0.0, 0.0, 0.0],
-            [1.8, 1.03923048, 0.0],
-            [3.6, 2.07846097, 0.0],
-            [0.0, 2.07846097, 2.54950976],
-            [1.8, 3.11769145, 2.54950976]
+            [ 1.27279221,  0.73484692,  0.        ],
+            [ 3.81837662,  0.73484692,  0.        ],
+            [ 6.36396103,  0.73484692,  0.        ],
+            [ 8.90954544,  0.73484692,  0.        ],
+            [11.45512986,  0.73484692,  0.        ]
             # ... possible to add other positions ...
         ])
         
@@ -25,8 +28,9 @@ class TestCoordinates(unittest.TestCase):
         # Verify that the first n positions are equal to the exected ones (with a certain tolerance)
         np.testing.assert_almost_equal(coords[:len(expected_positions)], expected_positions, decimal=6)
     
+    # Test of few properties of fcc lattice
     def test_fcc_properties(self):
-        # Test of few properties of fcc lattice
+        
         coords = Coordinates()
         d = np.linalg.norm(coords[0] - coords[1])  # distance between nearest negihbours
         expected_d = 3.6 / np.sqrt(2)
@@ -38,5 +42,6 @@ class TestCoordinates(unittest.TestCase):
         angle = np.arccos(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
         self.assertAlmostEqual(angle, 120 * np.pi / 180, places=6)
 
-if _name_ == '_main_':
+#if the file is correctly executed then tests are executed
+if __name__ == '__main__':
     unittest.main()
