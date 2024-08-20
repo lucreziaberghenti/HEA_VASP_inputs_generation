3124e5205cbf9216161806185c36a43d4ea4d14f
lucreziaberghenti <lucreziaberghenti@gmail.com>
2024-08-20 12:04:45 +0200

    Comments in docstring format

--- tests/test_VASP_input.py
+++ tests/test_VASP_input.py
@@ -5,18 +5,29 @@ from functions import VASP_input
 import numpy as np
 
 class TestVASPInput(unittest.TestCase):
-   
+    """
+    Unit test class for the VASP_input function. It uses unittest and mocking
+    to verify the behavior of the VASP_input function in different scenarios.
+    """
+
     @patch('functions.load_incar_settings')
     @patch('functions.load_kpoints_settings')
     @patch('functions.load_pseudo_setup')
     @patch('functions.os')
     @patch('functions.Vasp')
 
-
-
+    
     def test_vasp_input(self, mock_vasp, mock_os, mock_load_pseudo_setup,
                         mock_load_kpoints_settings, mock_load_incar_settings):
-        
+        """
+        Test case for the VASP_input function. It checks the following:
+        1. That the correct INCAR, KPOINTS, and POTCAR settings are loaded.
+        2. That the environment variable 'VASP_PP_PATH' is set correctly.
+        3. That the Vasp calculator is instantiated with the correct arguments.
+        4. That the write_input method is called with an Atoms object containing
+           the correct atomic species, positions, and cell dimensions.
+        """
+
         # setup mock return values
         mock_load_incar_settings.return_value = {'encut': 400, 'ismear': 0, 'sigma': 0.1}
         mock_load_kpoints_settings.return_value = {'kpts': [4, 4, 1]}
@@ -58,10 +69,11 @@ class TestVASPInput(unittest.TestCase):
         # verify the Atoms object
         self.assertEqual(atoms_arg.get_chemical_symbols(), species)
         self.assertTrue((atoms_arg.get_positions() == positions).all())
-        self.assertTrue(np.all(np.isclose(atoms_arg.get_cell(), [[12.727922061357855, 0.0, 0.0],[0.0, 8.81816307401944, 0.0],[0.0, 0.0, 6.235382907247957]])))
+        self.assertTrue(np.all(np.isclose(atoms_arg.get_cell(), [[12.727922061357855, 0.0, 0.0],
+                                                                 [0.0, 8.81816307401944, 0.0],
+                                                                 [0.0, 0.0, 6.235382907247957]])))
         self.assertTrue(np.all(atoms_arg.get_pbc()))
 
-#if the file is correctly executed then tests are executed
+# If the file is correctly executed, then tests are executed
 if __name__ == '__main__':
     unittest.main()
-
