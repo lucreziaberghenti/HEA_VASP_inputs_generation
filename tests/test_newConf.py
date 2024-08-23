97d28feeef91e215e12a0debf6c33086d0c751e8
lucreziaberghenti <lucreziaberghenti@gmail.com>
2024-08-23 14:54:09 +0200

    added input parameters to saved also in other files

--- tests/test_newConf.py
+++ tests/test_newConf.py
@@ -13,7 +13,7 @@ class Test_newConf(unittest.TestCase):
         Test to check that the generated configuration has the correct dimensions (3, 4, 5).
         """
         saved = []
-        conf = newConf(saved)
+        conf = newConf(saved, 3, 4, 5)
         self.assertEqual(conf.shape, (3, 4, 5), "Wrong dimensions")
    
     def test_generate_new_conf_distribution(self):
@@ -22,7 +22,7 @@ class Test_newConf(unittest.TestCase):
         with 12 occurrences of each element.
         """
         saved = []
-        conf = newConf(saved)
+        conf = newConf(saved, 3, 4, 5)
        
         n_expected = np.array([12, 12, 12, 12, 12])
         n_generated = np.array([np.sum(conf == i) for i in range(1, 6)])
@@ -47,7 +47,7 @@ class Test_newConf(unittest.TestCase):
                             [1, 2, 3, 4, 5],
                             [1, 2, 3, 4, 5]]])]
        
-        conf = newConf(saved)
+        conf = newConf(saved, 3, 4, 5)
        
         self.assertFalse(Equivalent(conf, saved), "Generated configuration is equivalent to one already saved")
 
@@ -56,11 +56,24 @@ class Test_newConf(unittest.TestCase):
         Test to check that multiple calls to newConf generate different configurations.
         """
         saved = []
-        conf1 = newConf(saved)
+        conf1 = newConf(saved, 3, 4, 5)
         saved.append(conf1)
-        conf2 = newConf(saved)
+        conf2 = newConf(saved, 3, 4, 5)
        
         self.assertFalse(np.array_equal(conf1, conf2), "The two saved configurations should be different")
+
+    def test_smaller_matrices(self):
+        """
+        Test to ensure that the generated configuration is not equivalent
+        to any of the configurations already saved even in the case of smaller matrices
+        """
+
+        saved = [np.array([[[1,2,3,4,5]]])]
+       
+        conf = newConf(saved, 1, 1, 5)
+       
+        self.assertFalse(np.array_equal(saved, conf), "The two saved configurations should be different")
+
 
 if __name__ == '__main__':
     """
